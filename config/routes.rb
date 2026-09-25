344d6e45c1f3cd38775e87f9c3aded8e9ad3d7f7
mayu-koike <mayukoike204@gmail.com>
2026-09-25 20:16:22 +0900

    TopControllerのrequest specが失敗する問題を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get "/", to: "top#home"
-
+  get "/home", to: "top#home"
+  root "top#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
+  let(:user) { create(:user) }
   describe "GET /home" do
+    before { sign_in user }
     it "returns http success" do
-      get "/top/home"
+      get "/home"
       expect(response).to have_http_status(:success)
     end
   end
